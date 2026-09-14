477c3dc4e924fb7d608e051dfe9ce6127673ae97
ssugary <ssugary0@gmail.com>
2026-09-14 17:21:21 -0300

    fix: loaded meshes are now of the correct type

--- src/core/ObjectParser.js
+++ src/core/ObjectParser.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { FileLoader } from './utils/FileLoader.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+
 export class ObjectParser
 {
     /**  Parses the type of the object and inserts it into the scene*/
@@ -18,10 +20,21 @@ export class ObjectParser
             FileLoader.getfile().then((contents) => 
             {
                 const objLoader = new OBJLoader();
-                const object = objLoader.parse(contents);
-                object.position.y = 0.5;
-                object.position.x = 0.5;
-                engine.sceneManager.addObject(object);
+                const group = objLoader.parse(contents);
+
+                group.traverse((child) => 
+                {
+                    if (child.isMesh) 
+                    {
+                        if (!child.geometry.index) 
+                            child.geometry = BufferGeometryUtils.mergeVertices(child.geometry);
+                        
+                        child.position.set(0.5, 0.5, 0);
+                        
+                        child.material = new THREE.MeshStandardMaterial({ color: 0x88ccff });                        
+                        engine.sceneManager.addObject(child);
+                    }
+                })
             }).catch((err) => 
             {
                 console.error('Error loading model: ', err);
